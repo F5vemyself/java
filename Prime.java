b457c71c5197576fecf376089f44d31ca9b08c13
Liu sichun <592578839@qq.com>
2018-10-12 10:25:26 +0800

    初始版本

---
+++ Prime.java
@@ -0,0 +1,26 @@
+package lsc.prime.number;
+
+public class Prime {
+
+	public void ifPrime(int m) {
+		int i,j;
+		for(j=2;j<=m;j++)
+		{
+			for(i=2;i<=j;i++)
+			{
+				if(j%i==0)
+					break;
+			}
+			if(i<j)
+				System.out.println(j+"不是素数");
+			else
+				System.out.println(j+"是素数");
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		Prime p=new Prime();
+		p.ifPrime(5);
+	}
+}
